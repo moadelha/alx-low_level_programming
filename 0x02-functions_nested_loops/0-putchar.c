0755ed317f798c6a74124791bfce55cc6558d394
= <=>
2023-09-12 12:51:11 -0700

    tet 2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "Holberton";
+	int i;
 
-int main(void){
-	printf("_putchar");
-	return 0;
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
